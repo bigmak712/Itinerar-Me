f340f0e0199e3517c9d9ba2656a80df7bf48f2f3
bigmak712 <bigmak712@gmail.com>
2017-04-17 21:44:19 -0700

    Added People Delegate Methods

--- Itinerar-Me/PeopleViewController.swift
+++ Itinerar-Me/PeopleViewController.swift
@@ -14,7 +14,7 @@ class PeopleViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     @IBOutlet weak var peopleDropDown: UIPickerView!
     
     // List for drop down menu
-    var list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10+" ]
+    var list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10+"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +27,28 @@ class PeopleViewController: UIViewController, UIPickerViewDelegate, UIPickerView
         // Dispose of any resources that can be recreated.
     }
     
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        self.view.endEditing(true)
+        return list[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.peopleTextField.text = self.list[row]
+        self.peopleDropDown.isHidden = true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        <#code#>
+    }
     
 
     /*
