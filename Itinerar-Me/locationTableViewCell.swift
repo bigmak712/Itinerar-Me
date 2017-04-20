0c2758f29ecd92def69c5f78ac95bab19c8a219c
Sarah Gemperle <sgemperl@ucsd.edu>
2017-04-19 19:55:02 -0700

    Location layout

---
+++ Itinerar-Me/locationTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  locationTableViewCell.swift
+//  Itinerar-Me
+//
+//  Created by Sarah Gemperle on 4/19/17.
+//  Copyright © 2017 ItinerarMe. All rights reserved.
+//
+
+import UIKit
+
+class locationTableViewCell: UICollectionViewCell {
+    
+    var locationName: String!
+    
+}
