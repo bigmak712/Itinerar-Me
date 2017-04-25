fff025ca5cd53ace2e706ee2d18f9d34c42e6949
vctang <vickytang13@gmail.com>
2017-04-24 20:41:55 -0700

    TimeVC
    Added in methods to change labels with the time change

--- Itinerar-Me/TimeViewController.swift
+++ Itinerar-Me/TimeViewController.swift
@@ -18,12 +18,31 @@ class TimeViewController: UIViewController {
 
     @IBOutlet weak var startTimePicker: UIDatePicker!
     @IBOutlet weak var endTimePicker: UIDatePicker!
+    @IBOutlet weak var startTimeLabel: UILabel!
+    @IBOutlet weak var endTimeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         startTimePicker.datePickerMode = .time
         endTimePicker.datePickerMode = .time
-
+    }
+    
+    @IBAction func startTimeChanged(_ sender: Any) {
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        let timeAsString = dateFormatter.stringFromDate(dateOnPicker)
+        //dateFormatter.dateFormat = "HH:mm"
+        //var strDate = dateFormatter.stringFromDate(myDatePicker.date)
+        self.startTimeLabel.text = timeAsString
+    }
+    
+    @IBAction func endTimeChanged(_ sender: Any) {
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        let timeAsString = dateFormatter.stringFromDate(dateOnPicker)
+        //dateFormatter.dateFormat = "HH:mm"
+        //var strDate = dateFormatter.stringFromDate(myDatePicker.date)
+        self.endTimeLabel.text = timeAsString
     }
 }
