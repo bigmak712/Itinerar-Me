f2cda70434948e77342e7765b37bc9c854ee0c13
bigmak712 <bigmak712@gmail.com>
2017-05-22 20:49:55 -0700

    got rid of Budget file

--- Itinerar-Me/NumberActivitiesViewController.swift
+++ Itinerar-Me/NumberActivitiesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NumberActivitiesViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+class NumberActivitiesViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var activitiesTextField: UITextField!
     @IBOutlet weak var restaurantsTextField: UITextField!
@@ -18,25 +18,11 @@ class NumberActivitiesViewController: UIViewController, UIPickerViewDelegate, UI
     
     let dark_green = UIColor(red: 12/255, green: 127/255, blue: 99/255, alpha: 1.0)
     
-    let pickerView = UIPickerView()
-    
-    var list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10+"]
-    var numActivities = 0
-    var numRestaurants = 0
-    
-    // 1: activities 2: restaurants
-    var selectedTextField = 1
-    
     var preferences: Preferences!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        pickerView.delegate = self
-        pickerView.dataSource = self
-        activitiesTextField.inputView = pickerView
-        restaurantsTextField.inputView = pickerView
-        
         backButton.layer.borderWidth = CGFloat(1.0)
         backButton.layer.cornerRadius = CGFloat(30.0)
         backButton.layer.borderColor = dark_green.cgColor
@@ -53,47 +39,35 @@ class NumberActivitiesViewController: UIViewController, UIPickerViewDelegate, UI
         super.viewDidLayoutSubviews()
     }
     
-    @IBAction func didEditActivities(_ sender: UITextField) {
-        selectedTextField = 1
+    func showAlert(title: String, message: String){
+        // create the alert
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
     }
 
-    @IBAction func didEditRestaurants(_ sender: UITextField) {
-        selectedTextField = 2
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return list[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return list.count
-    }
-
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
-        self.view.endEditing(true)
-        
-        if selectedTextField == 1 {
-            activitiesTextField.text = list[row]
-            numActivities = row + 1
-        }
-        else if selectedTextField == 2 {
-            restaurantsTextField.text = list[row]
-            numRestaurants = row + 1
-        }
-    }
     
     @IBAction func backButtonPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func onFinish(_ sender: Any) {
-        preferences.numOfActivities = numActivities
-        preferences.numOfRestaurants = numRestaurants
+        guard activitiesTextField.text != nil else {
+            showAlert(title: "Number of Activities Not Found", message: "Enter the Number of Activities")
+            return
+        }
+        
+        guard restaurantsTextField.text != nil else {
+            showAlert(title: "Number of Restaurants Not Found", message: "Enter the Number of Restaurants")
+            return
+        }
+        
+        preferences.numOfActivities = Int(activitiesTextField.text!)
+        preferences.numOfRestaurants = Int(restaurantsTextField.text!)
         
         let storyboard = UIStoryboard(name: "Selection", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "SelectionVC") as! SelectionViewController
